7c06defd967dd724e51b2c0afb961c796389ccb0
AnggaArindraShonta <angga.shonta@students.amikom.ac.id>
2021-06-04 14:43:24 +0700

    bottom

--- app/src/main/java/com/example/jitc/GetStartedActivity.java
+++ app/src/main/java/com/example/jitc/GetStartedActivity.java
@@ -2,13 +2,29 @@ package com.example.jitc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        imageButton = findViewById(R.id.btngetstarted);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetStartedActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jitc/SplashActivity.java
+++ app/src/main/java/com/example/jitc/SplashActivity.java
@@ -2,13 +2,29 @@ package com.example.jitc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.ThreeBounce;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Handler handler = new Handler();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        handler.postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, GetStartedActivity.class);
+            finishAffinity();
+            startActivity(i);
+        }, 2500);
     }
 }

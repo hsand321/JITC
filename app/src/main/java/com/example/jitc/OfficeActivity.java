52dc471fef4ebf17cb2576adf1167ca73c81667e
Your Name <you@example.com>
2021-06-05 10:55:21 +0700

    v1

---
+++ app/src/main/java/com/example/jitc/OfficeActivity.java
@@ -0,0 +1,14 @@
+package com.example.jitc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfficeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office);
+    }
+}

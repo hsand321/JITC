b28bd2c20e78b121bd8441fe9dec1539125aae4c
Your Name <you@example.com>
2021-06-09 10:41:55 +0700

    collapsing

--- app/src/main/java/com/example/jitc/OfficeActivity.java
+++ app/src/main/java/com/example/jitc/OfficeActivity.java
@@ -1,8 +1,17 @@
 package com.example.jitc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.palette.graphics.Palette;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.Objects;
 
 public class OfficeActivity extends AppCompatActivity {
 
@@ -10,5 +19,23 @@ public class OfficeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_office);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.office);
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(@Nullable Palette palette) {
+                if (palette !=null){
+                    collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(R.attr.colorPrimary));
+                }
+            }
+        });
+
+
     }
 }

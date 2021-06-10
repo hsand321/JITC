dca4dddf19fb9aeb72afeffdd2cdc4c7d50a0454
Your Name <you@example.com>
2021-06-10 12:24:23 +0700

    office

--- app/src/main/java/com/example/jitc/HomeFragment.java
+++ app/src/main/java/com/example/jitc/HomeFragment.java
@@ -1,19 +1,39 @@
 package com.example.jitc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class HomeFragment extends Fragment {
+    ImageView database, office;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        database = view.findViewById(R.id.database);
+        office = view.findViewById(R.id.office);
+//        database.setOnClickListener(v -> {
+//            Intent intent = new Intent(getActivity(), DatabaseActivity.class);
+//            startActivity(intent);
+//        });
+        office.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), OfficeActivity.class);
+            startActivity(intent);
+        });
     }
 }

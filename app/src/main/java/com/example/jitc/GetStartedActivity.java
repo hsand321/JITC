62aed05ad6c0c44be2b9031c62998142cf270f7c
Your Name <you@example.com>
2021-06-15 16:19:06 +0700

    rearange

--- app/src/main/java/com/example/jitc/GetStartedActivity.java
+++ app/src/main/java/com/example/jitc/GetStartedActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 
-import com.example.jitc.ui.MainActivity;
-
 public class GetStartedActivity extends AppCompatActivity {
 
     ImageButton imageButton;
--- app/src/main/java/com/example/jitc/MainActivity.java
+++ app/src/main/java/com/example/jitc/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.jitc.ui;
+package com.example.jitc;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
